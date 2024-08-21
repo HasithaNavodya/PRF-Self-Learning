1fca5b99258beea4a4932048da2ad66eca75e887
HAsithaNavodya <hasithanavodya@gmail.com>
2024-08-21 13:35:58 +0530

    Learn

---
+++ PRF/Example7.java
@@ -0,0 +1,19 @@
+class Example7 {
+	public static void main(String[]args){
+		System.out.println("10+20+30"); 				//Line1 
+		System.out.println("10+20"+"30");     			//Line2
+		System.out.println("10"+"20+30");     			//Line3
+		System.out.println("10"+"20"+"30");   			//Line4
+		System.out.println(10+"20"+"30");   			//Line5
+		System.out.println("10"+20+"30");     			//Line6
+		System.out.println("10"+"20"+30);     			//Line7
+		System.out.println(10+"20"+30);     			//Line8
+		System.out.println("10"+20+30);     			//Line9
+		System.out.println(10+20+"30");     			//Line10
+		System.out.println(10+20+30);					//Line11
+		System.out.println("10"+(20+30));     			//Line12
+		     	
+		
+	}
+}
+
